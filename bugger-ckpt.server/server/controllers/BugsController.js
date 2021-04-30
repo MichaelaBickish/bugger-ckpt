079f4261515868cee6446e85d3d034527cc969b8
Michaela Bickish <mmbickish@gmail.com>
2021-04-30 17:33:14 -0600

    friday update

--- bugger-ckpt.server/server/controllers/BugsController.js
+++ bugger-ckpt.server/server/controllers/BugsController.js
@@ -1,19 +1,34 @@
 import BaseController from '../utils/BaseController'
 import { Auth0Provider } from '@bcwdev/auth0provider'
+import { bugsService } from '../services/BugsService'
 
 export class BugsController extends BaseController {
   constructor() {
     super('api/bugs')
     this.router
       .get('', this.getAll)
-      // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
+      .get('/:id', this.findBug)
+    // .get('/:id/notes', getNotesByBugId)
       .use(Auth0Provider.getAuthorizedUserInfo)
+    // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
       .post('', this.create)
+      // .put('/:id', this.edit)
+      .delete('/:id', this.delete)
   }
 
   async getAll(req, res, next) {
     try {
-      return res.send(['value1', 'value2'])
+      const data = await bugsService.find({ creatorId: req.userInfo.id })
+      return res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async findBug(req, res, next) {
+    try {
+      const data = await bugsService.findById({ _id: req.params.id, creatorId: req.userInfo.id })
+      return res.send(data)
     } catch (error) {
       next(error)
     }
@@ -23,9 +38,21 @@ export class BugsController extends BaseController {
     try {
       // NOTE NEVER TRUST THE CLIENT TO ADD THE CREATOR ID
       req.body.creatorId = req.userInfo.id
-      res.send(req.body)
+      const data = await bugsService.create(req.body)
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      const data = await bugsService.delete(req.params.id, req.userInfo.id)
+      res.send(data)
     } catch (error) {
       next(error)
     }
   }
 }
+// in bug edit, invoke getone. var oldbug = . if old bug is closed, then no. if true, can edit.
+// edit: use an if statement to check is bug is closed.
--- bugger-ckpt.server/server/services/BugsService.js
+++ bugger-ckpt.server/server/services/BugsService.js
@@ -14,6 +14,23 @@ class BugsService {
     }
     return bug
   }
+
+  async create(body) {
+    return await dbContext.Bugs.create(body)
+  }
+
+  // delete- find by id and dont delete, but change bug.closed to true. and add closedDate in delete.
+  // another option: get the bug, check its property, flip the value, send it back.
+  async delete(id, userId) {
+    const bug = await this.findById(id)
+    if (bug.closed === false) {
+      bug.closed = true
+      const data = await dbContext.Bugs.findOneAndUpdate({ _id: id, creatorId: userId }, bug, { new: true })
+      return data
+    } else if (!bug) {
+      throw new BadRequest('Invalid Id')
+    }
+  }
 }
 
 export const bugsService = new BugsService()
