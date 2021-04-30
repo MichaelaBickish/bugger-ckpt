66535834ff4d2bd6da73bcd59f55399ee653c184
Michaela Bickish <mmbickish@gmail.com>
2021-04-30 15:01:58 -0600

    Init

--- bugger-ckpt.server/server/models/Note.js
+++ bugger-ckpt.server/server/models/Note.js
@@ -1,20 +1,21 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
 
-const Value = new Schema(
+const Note = new Schema(
   {
-    title: { type: String, required: true },
-    description: { type: String, required: true },
+    body: { type: String, required: true },
+    bugId: { type: ObjectId, ref: 'Bug', required: true },
     creatorId: { type: String, ref: 'Account', required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
 
-Value.virtual('creator', {
+Note.virtual('creator', {
   localField: 'creatorId',
   ref: 'Account',
   foreignField: '_id',
   justOne: true
 })
 
-export default Value
+export default Note
